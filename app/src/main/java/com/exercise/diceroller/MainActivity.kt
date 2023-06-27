6de494766042f76edde35749b374628912a3822f
rafaover <rafamsilva@gmail.com>
2023-06-27 10:27:58 +1000

    Project Creation - Updated MainActivity with related functions/composables

---
+++ app/src/main/java/com/exercise/diceroller/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.exercise.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.exercise.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme{
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun DiceWithButtonAndImage() {
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
+}
