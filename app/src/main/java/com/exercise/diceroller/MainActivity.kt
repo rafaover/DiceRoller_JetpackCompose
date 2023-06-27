9e0a016722d26a597429c547525999cef38da869
rafaover <rafamsilva@gmail.com>
2023-06-27 11:11:43 +1000

    Main - Added variable RESULT - added Image for Dice - Button onclick logic

--- app/src/main/java/com/exercise/diceroller/MainActivity.kt
+++ app/src/main/java/com/exercise/diceroller/MainActivity.kt
@@ -3,16 +3,22 @@ package com.exercise.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.exercise.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,11 +34,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result = 1
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Button(onClick = { /*TODO*/ }) {
+        Image(
+            painter = painterResource(R.drawable.dice_1),
+            contentDescription = "1",
+            modifier = Modifier.padding(bottom = 16.dp)
+
+        )
+        // Another solution instead of adding padding to the Dice, is adding a Spacer Composable.
+        // Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
             Text(stringResource(R.string.roll))
         }
     }
