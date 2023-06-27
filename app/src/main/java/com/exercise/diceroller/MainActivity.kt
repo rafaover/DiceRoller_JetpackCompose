d039cc3b099b93b8f80956294f71e5f7649365dc
rafaover <rafamsilva@gmail.com>
2023-06-27 10:46:30 +1000

    Main updates - Added composable for dice and buttons. - Added preview and modifiers.

--- app/src/main/java/com/exercise/diceroller/MainActivity.kt
+++ app/src/main/java/com/exercise/diceroller/MainActivity.kt
@@ -3,11 +3,11 @@ package com.exercise.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.exercise.diceroller.ui.theme.DiceRollerTheme
@@ -24,13 +24,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DiceWithButtonAndImage() {
-
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+    }
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun DiceRollerApp() {
-    DiceWithButtonAndImage()
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
 }
